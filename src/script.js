a2cae1fc06b7daa2f93a351d83fa353e920e8041
nk-on <johndoe@example.com>
2024-06-14 22:50:33 +0400

    Fix- wrapping count time function into closure in order to fix bug - which defined units as zeros everytime function  called this blocked properly increasing of time

--- src/script.js
+++ src/script.js
@@ -9,30 +9,33 @@ const stopButton = document.querySelector('[data-stop]');
 //if it will reach to 60 (1 minute) add 1 to minute container
 //it should pause after clicking stop button
 function countTime() {
- let seconds = (minutes = hours = 0);
-  seconds++;
-  if (seconds >= 60) {
-    minutes++;
-    seconds = 0;
-  }
-  if (minutes >= 60) {
-    hours++;
-    minutes = 0;
-  }
-  if (minutes >= 24) {
-    hours = 0;
-  }
-  seconds < 10
-    ? (secondsElement.textContent = `:0${seconds}`)
-    : (secondsElement.textContent = `:${seconds}`);
-  minutes < 10
-    ? (minutesElement.textContent = `:0${minutes}`)
-    : (minutesElement.textContent = `${minutes}`);
-  hours < 10
-    ? (hoursElement.textContent = `0${hours}`)
-    : (hoursElement.textContent = `${hours}`);
+  let seconds = (minutes = hours = 0);
+  return () => {
+    seconds++;
+    if (seconds >= 60) {
+      minutes++;
+      seconds = 0;
+    }
+    if (minutes >= 60) {
+      hours++;
+      minutes = 0;
+    }
+    if (minutes >= 24) {
+      hours = 0;
+    }
+    seconds < 10
+      ? (secondsElement.textContent = `:0${seconds}`)
+      : (secondsElement.textContent = `:${seconds}`);
+    minutes < 10
+      ? (minutesElement.textContent = `:0${minutes}`)
+      : (minutesElement.textContent = `${minutes}`);
+    hours < 10
+      ? (hoursElement.textContent = `0${hours}`)
+      : (hoursElement.textContent = `${hours}`);
+  };
 }
 function startStopWatch() {
-  setInterval(countTime, 1000);
+  const countTimeFun = countTime();
+  setInterval(countTimeFun, 1000);
 }
 startButton.addEventListener('click', startStopWatch);
